1ea40f3d3176efe0adc047cba191bb40ff7ca355
Melissa <hibnermm@gmail.com>
2024-01-22 17:53:14 -0800

    position icon over image

--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, Button} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
+import './event.css'
 
 //change button icons to only appear upon hover and overlap with image
 //<Card.Text as='div'> {event.organizerName} </Card.Text>
@@ -11,9 +12,11 @@ const Event = ({event}) => {
   return (
   <Card className= 'border border-0 my-3 mx-1 rounded-bottom-4 btn-light'>
     <Link to={`/event/${event._id}`}>
-    <Card.Img className="rounded" variant="top" src={event.image} />
+    <Card.Img className="rounded image" variant="top" src={event.image} />
+    <div className="icons">
       <Button variant="light"><i class="fa-regular fa-heart"></i></Button>
       <Button variant="light"><i class="fa-solid fa-share"></i></Button>
+      </div>
       </Link>
     <Card.Body>
     <Link to={`/event/${event._id}`}>
