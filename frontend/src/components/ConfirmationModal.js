2039b2dbd8fc7d8a8d114f0f7800f9259308c0a0
TrieaZon <108979358+TrieaZon@users.noreply.github.com>
2024-03-11 11:46:56 -0700

    added thank you message and disabled payment process in prep for deployment.

--- frontend/src/components/ConfirmationModal.js
+++ frontend/src/components/ConfirmationModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Row, Modal,
+import {Container, Modal, ListGroup, ListGroupItem,
 } from 'react-bootstrap';
 
 const ConfirmationModal = ({ setConfirmationShow }) => {
@@ -13,9 +13,37 @@ const ConfirmationModal = ({ setConfirmationShow }) => {
       <Modal.Body >
 
       <Container className="mx-auto flex-grow-1 d-flex justify-content-center">
-        <Row> 
-          <h3>Check your delivery email for your tickets!</h3>
-        </Row>
+        <ListGroup>
+          <ListGroupItem className='border border-0'>
+            <h3>Check your delivery email for your tickets!</h3>
+          </ListGroupItem>
+          <ListGroupItem className='border border-0 fs-4 fw-bold'>
+            <br />
+            <br />
+            <em>
+              "Thank you for exploring our Eventbrite learning clone!"
+            </em>
+            <br />
+            <br />
+            <em>
+              "Normally, the 'Confirm Order' button would take you through the paypal process.
+              However, because we can't guarantee everyone seeing this will use a paypal sandbox, we've disabled the process."
+            </em>
+            <br />
+            <br />
+            <em>
+              "We encourage you to take a look at our video demonstration in the Readme section in GitHub if you want to see the payment process through completion."
+            </em>
+            <br />
+            <br />
+            <br />
+            <em>
+              "Thanks again!"
+            </em>
+          </ListGroupItem>
+
+        </ListGroup>
+          
       </Container>
         
         
