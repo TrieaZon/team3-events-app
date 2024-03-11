fdd862f965012859dc6c53cdbfa52c8943199410
TrieaZon <108979358+TrieaZon@users.noreply.github.com>
2024-03-11 13:25:01 -0700

    prepped for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,34 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import eventRoutes from './routes/eventRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import errorHandler from './middleware/errorMiddleware.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import eventRoutes from './routes/eventRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import errorHandler from './middleware/errorMiddleware.js';
+import path from 'path';
 
-const app = express()
-dotenv.config()
-connectDB()
+const app = express();
+dotenv.config();
+connectDB();
 
 //convert to json
-app.use(express.json()) 
+app.use(express.json());
 
-app.use('/api/events', eventRoutes)
-app.use('/api/users', userRoutes)
+app.use('/api/events', eventRoutes);
+app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.get('/api/config/paypal', (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID)
-)
+);
 
-app.use(errorHandler)
-app.listen(8080, console.log('Server is running on port 8080'))
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
+app.use(errorHandler);
+const port = process.env.PORT;
+app.listen(port, console.log(`Server is running on port ${port}`));
